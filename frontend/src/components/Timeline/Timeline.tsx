74d252805a08c8773de478927978c3d01eea4b6c
asterbot <arjunsodhi7@gmail.com>
2025-08-30 01:46:20 +0530

    fixed timeline

--- frontend/src/components/Timeline/Timeline.tsx
+++ frontend/src/components/Timeline/Timeline.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import timelineEvents from './data/timelineData';
-
 import './Timeline.css';
 
-// Icons for timeline
+// // Icons for timeline
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import { TermType } from './types';
+
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import timelineEvents from './data/timelineData';
+
 
 function getColor(subject: string): string{
     // Get color of subject block
@@ -30,33 +33,32 @@ function getColor(subject: string): string{
 
 const Timeline: React.FC = () => {
   return (
-    <div className="timeline-container">
-      <h1 className="timeline-title">Timeline</h1>
-      <div className="timeline">
-        {timelineEvents.map((event, index) => (
-          <div key={index} className="timeline-item">
-            <div className="timeline-dot">
-              {event.termType === TermType.StudyTerm ? <SchoolOutlinedIcon /> : <WorkOutlineOutlinedIcon />}
-            </div>
-            <div className="timeline-content">
-              <div className="timeline-date">{event.date}</div>
-              <h3 className="timeline-item-title">{event.title}</h3>
-              <p className="timeline-description">{event.description}</p>
-              {event.courses.length > 0 && "Courses:"}
-                <ul className="list-group">
-                  {event.courses.map((course, index) => {
-                    return(
-                      <li key={index} className={"list-group-item list-group-item-" + getColor(course.subject)}>
-                        {course.subject} {course.courseCode}: {course.description}
-                      </li>
-                    )
-                  })}
-                </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <VerticalTimeline>
+      {timelineEvents.map((event, index) => (
+        <VerticalTimelineElement 
+          key={index}
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#0e0f0f', color: 'white', border:'2px solid rgb(33, 150, 243)' }}
+          date={event.date}
+          icon={event.termType === TermType.WorkTerm ? <WorkOutlineOutlinedIcon /> : <SchoolOutlinedIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">{event.title}</h3>
+            <p className="vertical-timeline-element-subtitle">{event.description}</p>
+            {event.courses.length!==0 ? <br />  : ""}
+            {event.courses.length!==0 ? "Courses:"  : ""}
+            <ul className='list-group'>
+              {event.courses.map((course, index) => {
+                return (
+                  <li key={index} className={"list-group-item list-group-item-" + getColor(course.subject)}>
+                     {course.subject} {course.courseCode}: {course.description}
+                   </li>
+                )
+              })}
+            </ul>
+
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
   );
 };
 
