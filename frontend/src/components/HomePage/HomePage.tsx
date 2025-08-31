ff61f6e893918e253a9a27a3c23d437cd016c52f
asterbot <arjunsodhi7@gmail.com>
2025-08-31 10:31:18 +0530

    better homepage

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -15,13 +15,14 @@ const HomePage: React.FC = () => {
 
       <div className="intro-box">
         <div className="intro-text">
-          <span className="question">Who am I?</span> Just someone who may get a CS degree one day :D
+          <span className="question">Who am I?</span> "Huge nerd" doesn't even scratch the surface.
           <br /><br />
-          I love making things with code, whether it's games, apps, system-level projects or anything in between, I'm always trying new things with software!
+          I love making things with code, whether it's games, apps, system-level projects or anything in between, I'm always trying new things with software!<br />
+          Check out <a href="/projects"><span className="link"> my projects  &raquo;</span></a>
           <br /><br />
           You can also check out my socials on the left and feel free to reach out :D
           <br /><br />
-          <span className="motto"><i>Coding my chaos!</i></span>
+          <span className="motto"><i>Coding my chaos, one commit at a time!</i></span>
         </div>
       </div>
     </div>
